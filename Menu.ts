39da877ec7c8748ba11f116b301c8da795000480
Isadora <isadoraloopes@hotmail.com>
2025-11-10 13:34:52 -0300

    Interface ContaRepository adicionada

--- Menu.ts
+++ Menu.ts
@@ -1,22 +1,13 @@
 import readlinesync = require("readline-sync");
-import { colors } from './src/util/Colors';
-import { Conta } from './src/model/Conta';
 import { ContaCorrente } from './src/model/ContaCorrente';
 import { ContaPoupanca } from './src/model/ContaPoupanca';
+import { colors } from './src/util/Colors';
 
 export function main() {
 
     let opcao: number;
-    
-// Objeto da Classe Conta (Teste)
-    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
-    conta.visualizar();
-    conta.sacar(10500);
-    conta.visualizar();
-    conta.depositar(5000);
-    conta.visualizar();
 
-// Objeto da Classe ContaCorrente (Teste)
+    // Objeto da Classe ContaCorrente (Teste)
     const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
     contacorrente.visualizar();
     contacorrente.sacar(2000);
@@ -24,7 +15,7 @@ export function main() {
     contacorrente.depositar(1000);
     contacorrente.visualizar();
 
-// Objeto da Classe ContaPoupanca (teste)
+    // Objeto da Classe ContaPoupanca (teste)
     const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
     contapoupanca.visualizar();
     contapoupanca.sacar(200);
@@ -118,7 +109,7 @@ export function main() {
 /* Função com os dados da pessoa desenvolvedora */
 function sobre(): void {
     console.log("\n*****************************************************");
-    console.log("Projeto Desenvolvido por: Isadora Lopes");
+    console.log("Projeto Desenvolvido por: ");
     console.log("Generation Brasil - isadoral@genstudents.org");
     console.log("https://github.com/Isadora-Lopes-Santos/Conta_Bancaria-ts");
     console.log("*****************************************************");
